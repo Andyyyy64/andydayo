fbab3e5f6aa9fc6bcbd74848a09bb5982d248df6
Your Name <takuyarossi77@gmail.com>
2022-05-28 16:38:48 +0900

    changed to Markdown Editer

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,62 @@
-
 <script>
+import { marked } from 'marked'
+import { debounce } from 'lodash-es'
+
 export default {
-  data() {
-    return {
-      text: 'Edit me',
-      checked: true,
-      checkedNames: ['Jack'],
-      picked: 'One',
-      selected: 'A',
-      multiSelected: ['A']
+  data: () => ({
+    input: '# hello'
+  }),
+  computed: {
+    output() {
+      return marked(this.input)
     }
+  },
+  methods: {
+    update: debounce(function (e) {
+      this.input = e.target.value
+    }, 100)
   }
 }
 </script>
 
 <template>
-  <h2>Text Input</h2>
-  <input v-model="text"> {{ text }}
-
-  <h2>Checkbox</h2>
-  <input type="checkbox" id="checkbox" v-model="checked">
-  <label for="checkbox">Checked: {{ checked }}</label>
-  
-  <h2>Multi Checkbox</h2>
-  <input type="checkbox" id="jack" value="Jack" v-model="checkedNames">
-  <label for="jack">Jack</label>
-  <input type="checkbox" id="john" value="John" v-model="checkedNames">
-  <label for="john">John</label>
-  <input type="checkbox" id="mike" value="Mike" v-model="checkedNames">
-  <label for="mike">Mike</label>
-  <p>Checked names: <pre>{{ checkedNames }}</pre></p>
-
-  <h2>Radio</h2>
-  <input type="radio" id="one" value="One" v-model="picked">
-  <label for="one">One</label>
-  <br>
-  <input type="radio" id="two" value="Two" v-model="picked">
-  <label for="two">Two</label>
-  <br>
-  <span>Picked: {{ picked }}</span>
-
-  <h2>Select</h2>
-  <select v-model="selected">
-    <option disabled value="">Please select one</option>
-    <option>A</option>
-    <option>B</option>
-    <option>C</option>
-  </select>
-  <span>Selected: {{ selected }}</span>
-
-  <h2>Multi Select</h2>
-  <select v-model="multiSelected" multiple style="width:100px">
-    <option>A</option>
-    <option>B</option>
-    <option>C</option>
-  </select>
-  <span>Selected: {{ multiSelected }}</span>
+  <div class="editor">
+    <textarea class="input" :value="input" @input="update"></textarea>
+    <div class="output" v-html="output"></div>
+  </div>
 </template>
+
+<style>
+body {
+  margin: 0;
+}
+
+.editor {
+  height: 100vh;
+  display: flex;
+}
+
+.input,
+.output {
+  overflow: auto;
+  width: 50%;
+  height: 100%;
+  box-sizing: border-box;
+  padding: 0 20px;
+}
+
+.input {
+  border: none;
+  border-right: 1px solid #ccc;
+  resize: none;
+  outline: none;
+  background-color: #f6f6f6;
+  font-size: 14px;
+  font-family: 'Monaco', courier, monospace;
+  padding: 20px;
+}
+
+code {
+  color: #f66;
+}
+</style>
