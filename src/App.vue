5e4b5e5a987a86437c5d495436b12342d72b7200
Your Name <takuyarossi77@gmail.com>
2022-05-28 19:13:39 +0900

    エラーがあってので試し

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,61 @@
 <script>
-import { marked } from 'marked'
-import { debounce } from 'lodash-es'
+cosnt API_URL =
+`https://api.github.com/repos/vuejs/core/commits?per_page=3&sha=`
 
 export default {
-  data: () => ({
-    input: '# hello'
+  data: ()=>({
+    branches:["main","v2-compat"],
+    currentBranch: "main",
+    commits:null
   }),
-  computed: {
-    output() {
-      return marked(this.input)
-    }
+
+  created(){
+    this.fetchData()
+  },
+
+  watch: {
+    currentBranch: "fecthdata"
   },
   methods: {
-    update: debounce(function (e) {
-      this.input = e.target.value
-    }, 100)
+    async fetchData(){
+      const url =`${API_URL}${this.currentBranch}`
+      this.commits=await(await fetch(url)).json()
+    },
+    truncate(v){
+      const newline=v.indexOf("\n")
+      return newline > 0 ? v.slice(0,newline) :v
+    },
+    formatData(v){
+      return v.replace(/T|Z/g, ' ')
+    }
   }
 }
 </script>
 
 <template>
-  <div class="editor">
-    <textarea class="input" :value="input" @input="update"></textarea>
-    <div class="output" v-html="output"></div>
+<div>
+  <h1>Latest Vue Core commits</h1>
+  <template v-for="branch in branches">
+  <div>
+    <input type="radio"
+    :id="branch"
+    :value="branch"
+    name="branch"
+    v-model="currentBranch">
+    <label :for="branch">{{branch}}</label>
   </div>
+  </template>
+  <p>Vuejs/vue@{{currentBranch}}</p>
+  <ul>
+    <li v-for="{heml_url,sha,author,commit} in commits">
+    <a :href="html_url" target="_blank" class="commit">{{sha.slice(0,7)}}</a>
+    -<span class="message">{{truncate(commit.message)}}</span><br>
+    by <span class="author">
+      <a :href="author_html_url" target="_blank">
+      {{commit.author.name}}</a>
+    </span>
+    at <span class="date">{{formatData(commit.author.data)}}</span>
+    </li>
+  </ul>
+</div>
 </template>
-
-<style>
-body {
-  margin: 0;
-}
-
-.editor {
-  height: 100vh;
-  display: flex;
-}
-
-.input,
-.output {
-  overflow: auto;
-  width: 50%;
-  height: 100%;
-  box-sizing: border-box;
-  padding: 0 20px;
-}
-
-.input {
-  border: none;
-  border-right: 1px solid #ccc;
-  resize: none;
-  outline: none;
-  background-color: #f6f6f6;
-  font-size: 14px;
-  font-family: 'Monaco', courier, monospace;
-  padding: 20px;
-}
-
-code {
-  color: #f66;
-}
-</style>
